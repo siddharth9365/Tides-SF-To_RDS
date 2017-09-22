93ae94f1541c9a83bc51ab28e55748037f0b7f29
shrinesoft5\shrine software <siddharth@shrinesoft.in>
2017-09-22 17:59:18 +0530

    last commit.....

--- SfToRDS/src/com/connecter/observers/CallObserver.java
+++
@@ -1,9 +0,0 @@
-package com.connecter.observers;
-
-import java.util.List;
-
-import com.connecter.bean.CallBean;
-
-public interface CallObserver {
-	public void insertOrUpdate(List<CallBean> callBeanList,Boolean isInsert,Boolean isUpdate,Boolean isBefore,Boolean isAfter);
-}
--- SfToRDS/src/com/connecter/subjects/CallSubject.java
+++
@@ -1,9 +0,0 @@
-package com.connecter.subjects;
-
-import com.connecter.observers.CallObserver;
-
-public interface CallSubject<T> {
-	public void registerObserver(CallObserver callObserver);
-    public void removeObserver(CallObserver callObserver);
-    public void notifyCallObservers();
-}
